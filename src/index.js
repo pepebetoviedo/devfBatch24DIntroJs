d9b667fbb77252e1ab11516d2ad566210270ee5f
pepebetoviedo <jaoc77@gmail.com>
2023-03-14 19:35:29 -0600

    clase 2

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//alert("Hola soy un comentario de una linea!");
+/*
+console.log("Hola");
+console.log("soy");
+console.log("un comentario multininea");
+*/
+
+//Un solo simbolo es un caracter
+//Los textos en programación se conocen como "strings" o cadenas de caracteres
+//Cuando unimos varios strings con el simbilo de +, se dice que estmos concatenando strings
+document.write("Titulo del documento" + "<br />");
+document.write("Otro texto del documento");
+
+//prompt("Dime tu nombre");
+
+console.log("Hola ", prompt("Dime tu nombre"));
---
+++ src/variables.js
@@ -0,0 +1,19 @@
+//1. declara una variable, solo una vez
+var a;
+
+//2. se puede asignar un valor despues
+a = 2;
+
+//3. Reasignarla un valor a la variable
+a = 8;
+
+//4. se declara e inicializa una variable
+var b = 5;
+
+//5. Redeclarar una variable
+var c= 8;
+var c= 10;
+
+
+var suma = b + a;
+console.log(suma);
